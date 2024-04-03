286eb9845b32fc74024c7a5d613f25f2c83d25f7
Andrew Leon Tepixtle <redwoodtrees4@outlook.com>
2024-04-02 23:03:05 -0400

    feat(pub-002): Material styled - Styled input boxes and text fields - Formatted output

--- src/PublicationCounter.js
+++ src/PublicationCounter.js
@@ -2,7 +2,12 @@ import React, { useState } from "react";
 import "./PublicationCounter.css";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
-import Divider from '@mui/material/Divider';
+import Divider from "@mui/material/Divider";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormHelperText from "@mui/material/FormHelperText";
+import FormControl from "@mui/material/FormControl";
+import Typography from "@mui/material/Typography";
 
 const PublicationCounter = () => {
   const [unitOfMeasureWeight, setUnitOfMeasureWeight] = useState(0);
@@ -35,7 +40,7 @@ const PublicationCounter = () => {
 
   return (
     <div>
-      <Stack 
+      <Stack
         direction="column"
         divider={<Divider orientation="vertical" flexItem />}
         spacing={2}
@@ -43,34 +48,76 @@ const PublicationCounter = () => {
         alignItems="center"
       >
         <h1>Publication Counter</h1>
-        <label>
-          Unit of Measure Weight:
-          <input
-            type="number"
-            value={unitOfMeasureWeight}
-            onChange={handleUnitOfMeasureWeightChange}
-          />
-        </label>
-        <label>
-          Total Weight of Batch:
-          <input
-            type="number"
-            value={totalWeightOfBatch}
-            onChange={handleInputChange}
-          />
-        </label>
-        <label>
-          Count in Unit of Measure:
-          <input
-            type="number"
-            value={countInUnitOfMeasure}
-            onChange={(e) => setCountInUnitOfMeasure(e.target.value)}
-          />
-        </label>
+        <Stack direction="row" spacing={2}>
+          <Typography variant="overline" display="block" gutterBottom>
+            Enter Unit of Measure
+          </Typography>
+          <FormControl sx={{ m: 0, width: "13ch" }} variant="outlined">
+            <OutlinedInput
+              id="outlined-adornment-weight"
+              type="number"
+              size="small"
+              value={unitOfMeasureWeight}
+              onChange={handleUnitOfMeasureWeightChange}
+              endAdornment={<InputAdornment position="end">g</InputAdornment>}
+              aria-describedby="outlined-weight-helper-text"
+              inputProps={{
+                "aria-label": "weight",
+              }}
+            />
+            <FormHelperText id="outlined-weight-helper-text">
+              Weight
+            </FormHelperText>
+          </FormControl>
+        </Stack>
+        <Stack direction="row" spacing={2}>
+          <Typography variant="overline" display="block" gutterBottom>
+            Enter Total Weight of Batch
+          </Typography>
+          <FormControl sx={{ m: 0, width: "13ch" }} variant="outlined">
+            <OutlinedInput
+              id="outlined-adornment-weight"
+              type="number"
+              size="small"
+              value={totalWeightOfBatch}
+              onChange={handleInputChange}
+              endAdornment={<InputAdornment position="end">g</InputAdornment>}
+              aria-describedby="outlined-weight-helper-text"
+              inputProps={{
+                "aria-label": "weight",
+              }}
+            />
+            <FormHelperText id="outlined-weight-helper-text">
+              Weight
+            </FormHelperText>
+          </FormControl>
+        </Stack>
+        <Stack direction="row" spacing={2}>
+          <Typography variant="overline" display="block" gutterBottom>
+            Enter Count in Unit of Measure
+          </Typography>
+          <FormControl sx={{ m: 0, width: "13ch" }} variant="outlined">
+            <OutlinedInput
+              id="outlined-adornment-weight"
+              type="number"
+              size="small"
+              value={countInUnitOfMeasure}
+              onChange={(e) => setCountInUnitOfMeasure(e.target.value)}
+              endAdornment={<InputAdornment position="end">g</InputAdornment>}
+              aria-describedby="outlined-weight-helper-text"
+              inputProps={{
+                "aria-label": "count",
+              }}
+            />
+            <FormHelperText id="outlined-weight-helper-text">
+              Weight
+            </FormHelperText>
+          </FormControl>
+        </Stack>
         <Button variant="contained" onClick={calculatePublicationCount}>
           Calculate Publication Count
         </Button>
-        {publicationCount > 0 && <p>Publication Count: {publicationCount}</p>}
+        {publicationCount > 0 && <p>Publication Count: {publicationCount.toFixed(2)}</p>}
       </Stack>
     </div>
   );
